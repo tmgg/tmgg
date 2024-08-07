d6b95f86777919b866016d841369cab7974b2643
jiangtao <jiangtao.cctv@qq.com>
2024-08-07 09:01:19 +0800

    chore: 发布kettle

--- set-version.cmd
+++ set-version.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set new_version=0.1.7
+set new_version=0.1.8
 
 echo current dir is
 cd
--- web-monorepo/apps/tmgg-system-kettle/.umirc.ts
+++ web-monorepo/apps/tmgg-system-kettle/.umirc.ts
@@ -53,5 +53,5 @@ export default defineConfig({
    * 渲染和 url 解绑，html 文件放哪都能使用
    */
   mpa:{},
-  outputPath:"../../src/main/resources/static/job"
+  outputPath:"../../../tmgg-system-kettle/src/main/resources/static/kettle"
 });
