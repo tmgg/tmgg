7d8cd30625df2e90f4d22bff0f7eebecf5d9bdf9
jiangtao <jiangtao.cctv@qq.com>
2024-08-13 09:04:22 +0800

    chore: 版本设置脚本

--- tmgg-dev-utils/src/main/java/io/github/tmgg/SetVersionHandler.java
+++ tmgg-dev-utils/src/main/java/io/github/tmgg/SetVersionHandler.java
@@ -1,5 +1,6 @@
 package io.github.tmgg;
 
+import cn.moon.lang.json.JsonTool;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.Map;
 
 public class SetVersionHandler {
 
@@ -19,12 +21,10 @@ public class SetVersionHandler {
 
     public static void main(String[] args) throws IOException {
         SetVersionHandler sv = new SetVersionHandler(NEW_VERSION);
-        sv.changeMaven();
+        // sv.changeMaven();
         sv.changeNpm();
     }
 
-    private void changeNpm() {
-    }
 
     private void changeMaven() throws IOException {
         File file = new File(".");
@@ -74,8 +74,64 @@ public class SetVersionHandler {
 
     }
 
+    private void changeNpm() throws IOException {
+        File file = new File("web-monorepo");
+        Collection<File> pkgs = FileUtils.listFiles(file, new IOFileFilter() {
+            @Override
+            public boolean accept(File file) {
+                String name = file.getName();
+                return name.equals("package.json");
+            }
 
-    public SetVersionHandler(String version) {
+            @Override
+            public boolean accept(File file, String s) {
+                return false;
+            }
+        }, new IOFileFilter() {
+            @Override
+            public boolean accept(File file) {
+                String baseName = FilenameUtils.getBaseName(file.getName());
+                return baseName.startsWith("tmgg-") || baseName.equals("apps") || baseName.equals("packages");
+            }
+
+            @Override
+            public boolean accept(File file, String s) {
+                return false;
+            }
+        });
+
+        System.out.println("开始修改package.json的版本");
+
+        for (File pkg : pkgs) {
+            System.out.println(pkg);
+            String json = FileUtils.readFileToString(pkg, StandardCharsets.UTF_8);
+
+            Map<String, Object> map = JsonTool.jsonToMap(json);
+            map.put("version", version);
+
+
+            // "@tmgg/tmgg-base" : "0.1.13"
+
+            // 修改依赖
+
+            String[] deps = {"peerDependencies", "dependencies"};
+            for (String dep : deps) {
+                Map<String, Object> depMap = (Map<String, Object>) map.get(dep);
+                if (depMap == null) {
+                    continue;
+                }
+
+                if (depMap.containsKey("@tmgg/tmgg-base")) {
+                    depMap.put("@tmgg/tmgg-base", version);
+                }
+            }
+            json = JsonTool.toPrettyJsonQuietly(map);
+
+            FileUtils.writeStringToFile(pkg, json, StandardCharsets.UTF_8);
+        }
+    }
+
+    private SetVersionHandler(String version) {
         this.version = version;
     }
 
