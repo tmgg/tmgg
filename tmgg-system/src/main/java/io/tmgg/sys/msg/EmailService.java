eba8c100714c3abd9aa7c7d7e1605a756531fb9f
jiangtao <jiangtao.cctv@qq.com>
2024-08-13 15:43:43 +0800

    feat: 系统通知

--- tmgg-system/src/main/java/io/tmgg/sys/msg/EmailService.java
+++ tmgg-system/src/main/java/io/tmgg/sys/msg/EmailService.java
@@ -4,6 +4,7 @@ package io.tmgg.sys.msg;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.mail.MailAccount;
 import cn.hutool.extra.mail.MailUtil;
+import io.tmgg.SystemProperties;
 import io.tmgg.sys.consts.dao.SysConfigDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ public class EmailService  {
     @Resource
     SysConfigDao sysConfigDao;
 
+    @Resource
+    SystemProperties systemProperties;
+
     public void send(String to,String title, String content) {
         String from = sysConfigDao.findValueByCode("EMAIL-FROM");
         String pass = sysConfigDao.findValueByCode("EMAIL-PASS");
@@ -24,6 +28,12 @@ public class EmailService  {
             log.info("邮箱账号或密码未设置,忽略邮件发送 {}", title );
             return;
         }
+
+        String siteTitle = systemProperties.getSiteTitle();
+        if(siteTitle != null){
+            title = "【" + siteTitle + "】 " + title;
+        }
+
         MailAccount account = new MailAccount();
         account.setFrom(from);
         account.setPass(pass);
