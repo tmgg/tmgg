156432a7daacc7267387d76a748545f3e46a6a2c
jiangtao <jiangtao.cctv@qq.com>
2024-09-04 17:25:00 +0800

    发布版本，增加kettle，code，job支持

--- tmgg-dev-utils/src/main/java/io/github/tmgg/SetVersionHandler.java
+++ tmgg-dev-utils/src/main/java/io/github/tmgg/SetVersionHandler.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 public class SetVersionHandler {
 
-    public static final String NEW_VERSION = "0.1.17";
+    public static final String NEW_VERSION = "0.1.18";
 
 
     public static void main(String[] args) throws IOException {
