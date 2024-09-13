c4453aa6953b47df6844516a968fc9856a5f00de
jiangtao <jiangtao.mxvc@qq.com>
2024-09-13 22:46:36 +0800

    我的

--- web-monorepo/apps/tmgg-app-work/src/pages/mine/index.config.js
+++ web-monorepo/apps/tmgg-app-work/src/pages/mine/index.config.js
@@ -1,3 +1,4 @@
 export default definePageConfig({
-  navigationBarTitleText: '首页'
+  navigationBarTitleText: '我的',
+  backgroundColor:'#f5f5f5'
 })
--- web-monorepo/apps/tmgg-app-work/src/pages/mine/index.jsx
+++ web-monorepo/apps/tmgg-app-work/src/pages/mine/index.jsx
@@ -4,6 +4,7 @@ import './index.scss'
 import React, {Component} from "react";
 import MainTabs from "../../components/MainTabs";
 import {HttpUtil} from "@tmgg/tmgg-app-base";
+import {Cell, CellGroup, Image} from "@antmjs/vantui";
 
 
 export default class extends Component {
@@ -19,12 +20,31 @@ export default class extends Component {
   }
 
   render() {
-    return <View>
-      <Text>我的</Text>
+    return <View style={{padding: 24, background: '#f5f5f5'}}>
+      <View style={{background: '#fff', padding: 24}}>
+        <view style={{display: 'flex', alignItems: 'center', gap: 24}}>
+          <Image
+            round
+            width="60px"
+            height="60px"
+            src="https://img.yzcdn.cn/vant/cat.jpeg"
+          />
+          <View>
+            <View>{this.state.userInfo.name}</View>
+            <View>账号：{this.state.userInfo.account}</View>
+          </View>
+        </view>
+      </View>
 
-      账号：
+      <View style={{marginTop: 12}}>
+        <Cell title='我的待办' border></Cell>
+        <Cell title='消息提醒'></Cell>
+      </View>
 
-      {JSON.stringify(this.state.userInfo)}
+      <View style={{marginTop: 12}}>
+        <Cell title='系统设置'></Cell>
+        <Cell title='退出登录'></Cell>
+      </View>
 
       <MainTabs value={'mine'}></MainTabs>
     </View>;
