4489ec171a94ea24ebd4c82fa40f7b7ae4d5ed75
jiangtao <jiangtao.mxvc@qq.com>
2024-09-18 22:16:19 +0800

    忽略javadoc警告

--- tmgg-base/tmgg-commons-web/src/main/java/io/tmgg/lang/AesTool.java
+++ tmgg-base/tmgg-commons-web/src/main/java/io/tmgg/lang/AesTool.java
@@ -2,6 +2,9 @@ package io.tmgg.lang;
 
 import cn.hutool.crypto.SecureUtil;
 
+/**
+ * AES 加密解密工具
+ */
 public class AesTool {
     private static final String key = "61GmaSlKCd4@mxvc";
     public static String encryptHex(String text) {
