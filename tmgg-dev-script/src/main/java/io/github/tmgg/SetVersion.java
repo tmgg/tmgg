537e3ceaf38ffacdbac4900978851dc2786286bd
jiangtao <jiangtao.mxvc@qq.com>
2024-09-22 18:05:52 +0800

    去掉一些安全头设置

--- tmgg-dev-script/src/main/java/io/github/tmgg/SetVersion.java
+++ tmgg-dev-script/src/main/java/io/github/tmgg/SetVersion.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class SetVersion {
 
-    public static final String NEW_VERSION = "0.1.31";
+    public static final String NEW_VERSION = "0.1.32";
 
 
     public static void main(String[] args) throws IOException {
