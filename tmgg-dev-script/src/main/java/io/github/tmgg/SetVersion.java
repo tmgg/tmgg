03f1d9ccc05def013d49b9d84b6ec7cdb77f3636
jiangtao <jiangtao.cctv@qq.com>
2024-09-29 14:15:46 +0800

    连接池监控

--- tmgg-dev-script/src/main/java/io/github/tmgg/SetVersion.java
+++ tmgg-dev-script/src/main/java/io/github/tmgg/SetVersion.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class SetVersion {
 
-    public static final String NEW_VERSION = "0.1.33";
+    public static final String NEW_VERSION = "0.1.34";
 
 
     public static void main(String[] args) throws IOException {
