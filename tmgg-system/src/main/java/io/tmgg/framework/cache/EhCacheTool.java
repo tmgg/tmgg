9259c20fc9cea979142587d9049e77f307982309
hw <410518072@qq.com>
2024-10-23 00:14:41 +0800

    缓存

--- tmgg-system/src/main/java/io/tmgg/framework/cache/EhCacheTool.java
+++ tmgg-system/src/main/java/io/tmgg/framework/cache/EhCacheTool.java
@@ -3,7 +3,7 @@ package io.tmgg.framework.cache;
 import javax.cache.CacheManager;
 import javax.cache.Caching;
 
-public class CacheTool {
+public class EhCacheTool {
 
     public static void create(){
         CacheManager cacheManager = Caching.getCachingProvider().getCacheManager();
