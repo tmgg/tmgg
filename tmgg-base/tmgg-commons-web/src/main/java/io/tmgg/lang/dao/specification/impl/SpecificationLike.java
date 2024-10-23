29ba1aad86696e45fe729becdaf172a9757c1c70
jiangtao <jiangtao.cctv@qq.com>
2024-10-23 15:02:49 +0800

    简化

--- tmgg-base/tmgg-commons-web/src/main/java/io/tmgg/lang/dao/specification/impl/SpecificationLike.java
+++ tmgg-base/tmgg-commons-web/src/main/java/io/tmgg/lang/dao/specification/impl/SpecificationLike.java
@@ -5,6 +5,7 @@ import io.tmgg.lang.dao.specification.ExpressionTool;
 import jakarta.persistence.criteria.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.Assert;
 
 public class SpecificationLike<T> implements Specification<T> {
 
@@ -14,14 +15,12 @@ public class SpecificationLike<T> implements Specification<T> {
     public SpecificationLike(String fieldName, String value) {
         this.fieldName = fieldName;
         this.value = value;
+        Assert.hasText(value,"值不能为空");
     }
 
     @Override
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, @NotNull CriteriaBuilder builder) {
         Expression<T> expression = ExpressionTool.getExpression(fieldName, root);
-        if (StrUtil.isEmpty(value)) {
-            return null;
-        }
 
         String likeValue = value.contains("%") ? value : "%" + value + "%";
         return builder.like((Expression<String>) expression, likeValue);
