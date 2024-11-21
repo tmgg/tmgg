8b7cc677d9d6cc0f0b51178bbb767c5ed0b8ab08
jiangtao <jiangtao.cctv@qq.com>
2024-11-21 10:25:03 +0800

    去掉jwt，可以使用hutool的jwt

--- tmgg-base/tmgg-commons-web/src/main/java/io/tmgg/web/jwt/JwtInvalidException.java
+++
@@ -1,8 +0,0 @@
-package io.tmgg.web.jwt;
-
-
-public class JwtInvalidException extends IllegalStateException {
-    public JwtInvalidException(String msg) {
-        super(msg);
-    }
-}
--- tmgg-base/tmgg-commons-web/src/main/java/io/tmgg/web/jwt/JwtNotFountException.java
+++
@@ -1,9 +0,0 @@
-package io.tmgg.web.jwt;
-
-
-public class JwtNotFountException extends IllegalStateException {
-    public JwtNotFountException() {
-        super("未找到登录凭证(jwt is null)");
-    }
-}
-
