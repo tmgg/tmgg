1b36352ffb72891108d1f12e99a3b87bcb9d3d64
jiangtao <jiangtao.cctv@qq.com>
2024-11-27 09:51:52 +0800

    是否更新使用下划线

--- tmgg-system/src/main/java/io/tmgg/sys/msg/EmailService.java
+++ tmgg-system/src/main/java/io/tmgg/sys/msg/EmailService.java
@@ -4,7 +4,7 @@ package io.tmgg.sys.msg;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.mail.MailAccount;
 import cn.hutool.extra.mail.MailUtil;
-import io.tmgg.sys.dao.SysConfigDao;
+import io.tmgg.sys.service.SysConfigService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +17,13 @@ import java.io.File;
 public class EmailService  {
 
     @Resource
-    SysConfigDao sysConfigDao;
+    SysConfigService sysConfigService;
 
 
 
     public void send(String to,String title, String content, File... files) {
-        String from = sysConfigDao.findValueStr("email.from");
-        String pass = sysConfigDao.findValueStr("email.pass");
+        String from = sysConfigService.findValueStr("email.from");
+        String pass = sysConfigService.findValueStr("email.pass");
         if(StrUtil.isEmpty(from) || StrUtil.isEmpty(pass)){
             throw new IllegalStateException("邮箱账号或密码未设置,忽略邮件发送");
         }
--- tmgg-system/src/main/java/io/tmgg/sys/service/SysConfigService.java
+++ tmgg-system/src/main/java/io/tmgg/sys/service/SysConfigService.java
@@ -1,14 +1,21 @@
 
 package io.tmgg.sys.service;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.system.SystemUtil;
+import io.tmgg.SysProp;
+import io.tmgg.lang.SpringTool;
 import io.tmgg.lang.dao.BaseService;
+import io.tmgg.lang.dao.specification.JpaQuery;
 import io.tmgg.sys.dao.SysConfigDao;
 import io.tmgg.sys.entity.SysConfig;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -21,18 +28,18 @@ public class SysConfigService extends BaseService<SysConfig> {
 
     @Override
     public SysConfig saveOrUpdate(SysConfig input) throws Exception {
-        SysConfig old = baseDao.findOne(input);
+        SysConfig old = dao.findOne(input);
         old.setValue(input.getValue());
-        return baseDao.save(old);
+        return dao.save(old);
     }
 
     public boolean getBoolean(String key) {
-        Object value = dao.findValue(key);
+        Object value = this.findValue(key);
         return (boolean) value;
     }
 
     public String getStr(String key) {
-        return (String) dao.findValue(key);
+        return (String) this.findValue(key);
     }
 
 
@@ -60,7 +67,74 @@ public class SysConfigService extends BaseService<SysConfig> {
 
 
     public Map<String, Object> findSiteInfo() {
-        return dao.findByPrefix("siteInfo");
+        return this.findByPrefix("siteInfo");
     }
 
+
+    /**
+     * 通过前缀查询键值对
+     *
+     * @param prefix
+     * @return
+     */
+    public Map<String, Object> findByPrefix(String prefix) {
+        if (!prefix.endsWith(".")) {
+            prefix = prefix + ".";
+        }
+        JpaQuery<SysConfig> q = new JpaQuery<>();
+        q.like("id", prefix + "%");
+        List<SysConfig> list = this.findAll(q);
+
+        Map<String, Object> map = new HashMap<>();
+        for (SysConfig sysConfig : list) {
+            String k = sysConfig.getId();
+            k = k.replace(prefix, "");
+            Object v = parseFinalValue(sysConfig);
+            map.put(k, v);
+        }
+
+        return map;
+    }
+
+    // 解析最终的值， 优先级 数据库value > spring属性 > 默认值
+    private static Object parseFinalValue(SysConfig sysConfig) {
+        String v = sysConfig.getValue();
+        if (StrUtil.isEmpty(v)) {
+            // 环境变量
+            String propKey = SysProp.CONFIG_PREFIX + "." + sysConfig.getId();
+            propKey = StrUtil.toUnderlineCase(propKey).replace("_","-"); // 将大写转换为-
+
+            String property = SpringTool.getProperty(propKey);
+            if (StrUtil.isNotEmpty(property)) {
+                v = property;
+            }
+        }
+
+        if (StrUtil.isEmpty(v)) {
+            v = sysConfig.getDefaultValue();
+        }
+
+
+        if ("boolean".equals(sysConfig.getValueType())) {
+            return Boolean.valueOf(v);
+        }
+
+
+        return v;
+    }
+
+
+    public Object findValue(String key) {
+        SysConfig sysConfig = this.findOne(key);
+        Assert.notNull(sysConfig, "系统配置不存在" + key);
+        return parseFinalValue(sysConfig);
+    }
+
+    public String findValueStr(String key) {
+        SysConfig sysConfig = this.findOne(key);
+        if (sysConfig != null) {
+            return (String) parseFinalValue(sysConfig);
+        }
+        return null;
+    }
 }
