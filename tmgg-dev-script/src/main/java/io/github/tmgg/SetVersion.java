b2b0051b5778884af70e1368dcf01b724df63ea0
jiangtao <jiangtao.cctv@qq.com>
2024-11-27 17:20:22 +0800

    json美化，方便查看

--- tmgg-dev-script/src/main/java/io/github/tmgg/SetVersion.java
+++ tmgg-dev-script/src/main/java/io/github/tmgg/SetVersion.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 public class SetVersion {
 
-    public static final String NEW_VERSION = "0.1.67";
+    public static final String NEW_VERSION = "0.1.68";
 
 
     public static void main(String[] args) throws IOException {
