037184490390ab866956b8d126f61601e932a708
jiangtao <jiangtao.cctv@qq.com>
2024-12-20 15:59:56 +0800

    自动填充文件url

--- tmgg-system/src/main/java/io/tmgg/core/jpa/fill/AutoFillFileUrl.java
+++ tmgg-system/src/main/java/io/tmgg/core/jpa/fill/AutoFillFileUrl.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
-@AutoFill(value = AutoFillUserLabelStrategy.class)
+@AutoFill(value = AutoFillFileUrlStrategy.class)
 public @interface AutoFillFileUrl {
 
 }
