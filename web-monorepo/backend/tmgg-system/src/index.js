c66f0dca2f668b30330de81f4d1e7061ee86899d
jiangtao <jiangtao.cctv@qq.com>
2025-01-17 09:04:38 +0800

    路由错误

--- web-monorepo/backend/tmgg-system/src/index.js
+++ web-monorepo/backend/tmgg-system/src/index.js
@@ -8,7 +8,12 @@ import {registerRoutes} from "@tmgg/tmgg-base";
 export * from "./layouts"
 
 
-export function initSystem(){
+export function initSystem() {
+    /**
+     * 其实这里的路由不是框架用的，是项目调用的，
+     *
+     * 由于使用了umi，使用了基于文件的自动路由，实际项目集成是，需要手动调用一下
+     */
     registerRoutes({
         'settings/ChangePassword': require('./pages/settings/ChangePassword').default,
         'system/onlineUser': require('./pages/system/onlineUser/index').default,
@@ -20,7 +25,7 @@ export function initSystem(){
         'system/dict': require('./pages/system/dict/index').default,
         'system/role': require('./pages/system/role/index').default,
         'system/menu': require('./pages/system/menu/index').default,
-        'system/user': require( "./pages/system/user").default,
+        'system/user': require("./pages/system/user").default,
         'system/file/main': require('./pages/system/file/main').default,
         'system/msg': require('./pages/system/msg/index').default,
         'system/org': require('./pages/system/org/index').default,
@@ -31,7 +36,7 @@ export function initSystem(){
 
         'code': require('./pages/code').default,
 
-        'mysqlStatus': require('./pages/system/mysqlStatus').default,
+        'system/mysqlStatus': require('./pages/system/mysqlStatus').default,
 
         'job': require('./pages/job').default,
         'job/logList': require('./pages/job/logList').default,
