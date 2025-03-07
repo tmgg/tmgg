5cc08823975d28ff81e27810176d95fef4b00921
jiangtao <jiangtao.cctv@qq.com>
2025-03-07 09:35:46 +0800

    增加前端模板

---
+++ templates/frontend-template/src/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  animation: false,
+}
+)
