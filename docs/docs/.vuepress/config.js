2c55bc9315a4d75af61d9e0e3fb59c3cbacaa19f
hw <410518072@qq.com>
2025-03-11 20:05:06 +0800

    vue 文档

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+
+  title: '开发文档',
+  description: '一个后台管理框架',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+
+
+})
---
+++ docs/docs/README.md
@@ -0,0 +1,12 @@
+---
+home: false
+title: 首页
+---
+
+# xxx
+
+This is the content of home page. 
+
+## xyz
+
+xxxx
