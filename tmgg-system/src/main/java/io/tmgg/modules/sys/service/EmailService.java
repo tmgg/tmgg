3c7fe450a9c71a0f75ef6993a4f3c6bc119572dc
hw <410518072@qq.com>
2025-04-16 22:54:26 +0800

    简化邮箱配置

--- tmgg-system/src/main/java/io/tmgg/modules/sys/service/EmailService.java
+++ tmgg-system/src/main/java/io/tmgg/modules/sys/service/EmailService.java
@@ -22,11 +22,8 @@ public class EmailService  {
 
 
     public void send(String to,String title, String content, File... files) {
-        String from = sysConfigService.getStr("sys.email.from");
-        String pass = sysConfigService.getStr("sys.email.pass");
-        if(StrUtil.isEmpty(from) || StrUtil.isEmpty(pass)){
-            throw new IllegalStateException("邮箱账号或密码未设置,忽略邮件发送");
-        }
+        String from = sysConfigService.getStr("email.from");
+        String pass = sysConfigService.getStr("email.pass");
 
 
         MailAccount account = new MailAccount();
