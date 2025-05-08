9e7e6157e6d3126a5edf60c016a2a45aafb0978f
jiangtao <jiangtao.cctv@qq.com>
2025-05-08 17:29:27 +0800

    记录接口日志

--- web-monorepo/backend/tmgg-system/src/pages/api/accessLog/index.jsx
+++ web-monorepo/backend/tmgg-system/src/pages/api/accessLog/index.jsx
@@ -7,13 +7,7 @@ import {ButtonList,dictValueTag, ViewBoolean,FieldDateRange,FieldDictSelect,Fiel
 
 export default class extends React.Component {
 
-  state = {
-    formValues: {},
-    formOpen: false
-  }
 
-  formRef = React.createRef()
-  tableRef = React.createRef()
 
   columns = [
 
@@ -95,100 +89,19 @@ export default class extends React.Component {
 
     },
 
-    {
-      title: '操作',
-      dataIndex: 'option',
-      render: (_, record) => (
-          <ButtonList>
-            <Button size='small' perm='apiAccessLog:save' onClick={() => this.handleEdit(record)}>编辑</Button>
-            <Popconfirm perm='apiAccessLog:delete' title='是否确定删除接口访问记录'  onConfirm={() => this.handleDelete(record)}>
-              <Button size='small'>删除</Button>
-            </Popconfirm>
-          </ButtonList>
-      ),
-    },
+
   ]
 
-  handleAdd = ()=>{
-    this.setState({formOpen: true, formValues: {}})
-  }
 
-  handleEdit = record=>{
-      this.setState({formOpen: true, formValues: record})
-  }
-
-
-  onFinish = values => {
-    HttpUtil.post( 'apiAccessLog/save', values).then(rs => {
-      this.setState({formOpen: false})
-      this.tableRef.current.reload()
-    })
-  }
-
-
-
-  handleDelete = record => {
-    HttpUtil.postForm( 'apiAccessLog/delete', {id:record.id}).then(rs => {
-      this.tableRef.current.reload()
-    })
-  }
 
   render() {
     return <Page>
       <ProTable
-          actionRef={this.tableRef}
-          toolBarRender={() => {
-            return <ButtonList>
-              <Button perm='apiAccessLog:save' type='primary' onClick={this.handleAdd}>
-                <PlusOutlined/> 新增
-              </Button>
-            </ButtonList>
-          }}
           request={(params) => HttpUtil.pageData('apiAccessLog/page', params)}
           columns={this.columns}
       />
 
-  <Modal title='接口访问记录'
-    open={this.state.formOpen}
-    onOk={() => this.formRef.current.submit()}
-    onCancel={() => this.setState({formOpen: false})}
-    destroyOnClose
-    maskClosable={false}
-    >
 
-    <Form ref={this.formRef} labelCol={{flex: '100px'}}
-        initialValues={this.state.formValues}
-        onFinish={this.onFinish}
-        >
-        <Form.Item  name='id' noStyle></Form.Item>
-
-              <Form.Item label='接口名称' name='name' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-              <Form.Item label='接口' name='action' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-              <Form.Item label='请求数据' name='requestData' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-              <Form.Item label='响应数据' name='responseData' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-              <Form.Item label='ip' name='ip' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-              <Form.Item label='ipLocation' name='ipLocation' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-              <Form.Item label='执行时间' name='executionTime' rules={[{required: true}]}>
-                    <InputNumber />
-              </Form.Item>
-              <Form.Item label='接口账户' name='accountName' rules={[{required: true}]}>
-                    <Input/>
-              </Form.Item>
-
-    </Form>
-  </Modal>
     </Page>
 
 
