d8359ada47b342ae34c7fd1e8e0645f2c5bdbdb3
jiangtao <jiangtao.cctv@qq.com>
2025-05-09 14:19:36 +0800

    自动导出

--- tmgg-system/src/main/java/io/tmgg/modules/sys/entity/SysConfig.java
+++ tmgg-system/src/main/java/io/tmgg/modules/sys/entity/SysConfig.java
@@ -1,6 +1,7 @@
 
 package io.tmgg.modules.sys.entity;
 
+import io.tmgg.commons.poi.excel.annotation.Excel;
 import io.tmgg.lang.ann.Msg;
 import io.tmgg.lang.dao.BaseEntity;
 import lombok.Getter;
@@ -25,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 @ToString
 public class SysConfig extends BaseEntity {
 
+    @Excel(name = "名称")
     @Msg("名称")
     @Column(length = 30, nullable = false)
     private String label;
@@ -33,6 +35,7 @@ public class SysConfig extends BaseEntity {
     /**
      * 属性值
      */
+    @Excel(name = "值")
     @Msg("值")
     private String value;
 
