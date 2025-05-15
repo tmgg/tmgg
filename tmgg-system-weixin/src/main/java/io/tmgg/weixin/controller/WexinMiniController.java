0617675f6d16649c7349271a220eaf5dc6b35632
jiangtao <jiangtao.cctv@qq.com>
2025-05-15 16:05:28 +0800

    文件素材

--- tmgg-system-weixin/src/main/java/io/tmgg/weixin/controller/WexinMiniController.java
+++ tmgg-system-weixin/src/main/java/io/tmgg/weixin/controller/WexinMiniController.java
@@ -1,6 +1,6 @@
 package io.tmgg.weixin.controller;
 
-import io.tmgg.web.persistence.BaseCURDController;
+import io.tmgg.web.persistence.BaseController;
 import io.tmgg.weixin.entity.WeixinMini;
 import io.tmgg.weixin.service.WeixinMiniService;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +9,7 @@ import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("weixinMini")
-public class WexinMiniController extends BaseCURDController<WeixinMini> {
+public class WexinMiniController extends BaseController<WeixinMini> {
 
     @Resource
     private WeixinMiniService service;
