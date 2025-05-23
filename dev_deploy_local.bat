1e6c70d3a89a1283fce83862297c3a40a11a3d2e
jiangtao <jiangtao.cctv@qq.com>
2025-05-23 17:22:07 +0800

    测试脚本

--- dev_deploy_local.bat
+++ dev_deploy_local.bat
@@ -4,10 +4,9 @@ chcp 65001
 echo 将代码部署到本机，以便集成测试
 
 echo 执行 npm
-cd cd web-monorepo
-call pnpm publish -r --access public --no-git-checks --registry https://packages.aliyun.com/62d39be70065edd3d51c1984/npm/npm-registry/
+cd web-monorepo
+call  pnpm publish  -r --access public --no-git-checks --registry https://packages.aliyun.com/62d39be70065edd3d51c1984/npm/npm-registry/
 cd ..
-
 
 ::call mvn install -Drevision=1.1.1 -DskipTests  -B -T 1C
 
