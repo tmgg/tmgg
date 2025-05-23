a208527272dacc29867ecb4f38d8ca6590203fc2
jiangtao <jiangtao.cctv@qq.com>
2025-05-23 16:54:02 +0800

    测试脚本

--- dev_deploy_local.bat
+++ dev_deploy_local.bat
@@ -4,10 +4,13 @@ chcp 65001
 echo 将代码部署到本机，以便集成测试
 
 echo 正在执行 mvn
-call mvn install -Drevision=1.1.1
-echo '执行 mvn 完毕'
+call mvn install -Drevision=1.1.1 -DskipTests -q -B -T 1C
+echo 执行 mvn 完毕
 
 
 echo -------------------------------------------------------------------
-echo
-echo 执行pnpm
+echo -------------------------------------------------------------------
+echo 执行 npm
+cd web-monorepo
+call pnpm publish -r --access public --no-git-checks --registry https://packages.aliyun.com/62d39be70065edd3d51c1984/npm/npm-registry/
+cd ..
