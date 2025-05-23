e619f25b8879fd902cc44d83d657713ab240bbce
jiangtao <jiangtao.cctv@qq.com>
2025-05-23 17:28:12 +0800

    测试脚本

--- dev_deploy_local.bat
+++ dev_deploy_local.bat
@@ -8,6 +8,10 @@ cd web-monorepo
 call  pnpm publish  -r --access public --no-git-checks --registry https://packages.aliyun.com/62d39be70065edd3d51c1984/npm/npm-registry/
 cd ..
 
+
+echo -------------------------------------------------------------------
+echo -------------------------------------------------------------------
+
 call mvn clean deploy  -DskipTests  -B -T 1C
 
 
