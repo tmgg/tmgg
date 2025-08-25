c57be66cb53d978af5e53748c49e24f79c57ac89
jiangtao <jiangtao.cctv@qq.com>
2025-08-25 16:45:10 +0800

    gitpack

---
+++ version.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 > nul
+
+set /p "tag_name=请输入标签名称如1.1.x: "
+
+
+echo 正在删除本地标签（如果存在）...
+git tag -d %tag_name%
+
+echo 正在删除远程标签（如果存在）...
+git push origin --delete %tag_name%
+
+echo 正在创建新标签...
+git tag %tag_name%
+
+echo 正在推送标签到远程仓库...
+git push origin %tag_name%
+
+
+echo 标签 %tag_name% 已成功创建并推送
