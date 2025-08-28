3e9401768028788de96c39dd16666d23ea13b0f2
jiangtao <jiangtao.cctv@qq.com>
2025-08-28 17:09:41 +0800

    简化导出

--- script_push_tag.bat
+++ script_push_tag.bat
@@ -2,20 +2,16 @@
 chcp 65001 > nul
 
 
-set  "tag_name=v1.1.29"
+set  "tag_name=v1.1.30"
 
 
-echo 正在删除本地标签（如果存在）...
 git tag -d %tag_name%
 
-echo 正在删除远程标签（如果存在）...
 git push origin --delete %tag_name%
 
-echo 正在创建新标签...
 git tag %tag_name%
 
-echo 正在推送标签到远程仓库...
 git push origin %tag_name%
 
 
-echo 标签 %tag_name% 脚本执行完成
+echo  %tag_name% finished
