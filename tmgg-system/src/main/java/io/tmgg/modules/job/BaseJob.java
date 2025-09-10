11864cff1a49ef3abee7684b796518ba9c3a6d66
jiangtao <jiangtao.cctv@qq.com>
2025-09-10 09:41:32 +0800

    增加方法

---
+++ tmgg-system/src/main/java/io/tmgg/modules/job/BaseJob.java
@@ -0,0 +1,23 @@
+package io.tmgg.modules.job;
+
+import org.quartz.*;
+import org.slf4j.Logger;
+
+import java.util.Map;
+
+@DisallowConcurrentExecution // 不允许并发
+public abstract class BaseJob implements Job {
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        JobDataMap data = JobTool.getData(jobExecutionContext);
+        Logger logger = JobTool.getLogger();
+
+        String result = this.execute(data, logger);
+
+        jobExecutionContext.setResult(result);
+    }
+
+    public abstract String execute(JobDataMap data, Logger log);
+}
