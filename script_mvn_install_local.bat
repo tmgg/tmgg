d70ae548f3eabd94a8e19801f37c7db8c7c01b81
jiangtao <jiangtao.cctv@qq.com>
2025-09-12 10:11:13 +0800

    流程引擎

--- script_mvn_install_local.bat
+++ script_mvn_install_local.bat
@@ -1,9 +1,9 @@
 @echo off
-set  "tag_name=1.1.38"
+set  "tag_name=1.1.39"
 
 
 echo install mvn jar to local, version is %tag_name%
-./mvnw clean  install  -DskipTests -Drevision=%tag_name%
+call ./mvnw clean  install  -DskipTests -Drevision=%tag_name%
 
 
 echo  %tag_name% finished
