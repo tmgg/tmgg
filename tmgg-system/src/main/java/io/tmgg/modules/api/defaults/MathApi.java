37a893d1288069bc718891be14a2704071706f5f
jiangtao <jiangtao.cctv@qq.com>
2025-10-09 15:34:46 +0800

    调整接口

--- tmgg-system/src/main/java/io/tmgg/modules/api/defaults/MathApi.java
+++ tmgg-system/src/main/java/io/tmgg/modules/api/defaults/MathApi.java
@@ -1,9 +1,6 @@
 package io.tmgg.modules.api.defaults;
 
-import io.tmgg.lang.field.FieldInfo;
 import io.tmgg.modules.api.ApiMapping;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +8,7 @@ public class MathApi {
 
 
 
-	@ApiMapping(action = "add", name = "加法计算", desc = "根据输入参数a，b，计算a+b的结果")
+	@ApiMapping(action = "math.add", name = "加法计算", desc = "根据输入参数a，b，计算a+b的结果")
 	public int add(int a, int b){
 		return a +b;
 	}
