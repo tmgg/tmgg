e8b324eb0eb7fff21fcce2b2abf88f2ffb5c5142
jiangtao <jiangtao.cctv@qq.com>
2025-10-09 09:57:42 +0800

    调整接口路径

--- tmgg-system/src/main/java/io/tmgg/modules/job/quartz/QuartzInit.java
+++ tmgg-system/src/main/java/io/tmgg/modules/job/quartz/QuartzInit.java
@@ -1,6 +1,7 @@
 package io.tmgg.modules.job.quartz;
 
 import io.tmgg.config.SysProp;
+import io.tmgg.lang.SpringTool;
 import io.tmgg.modules.job.dao.SysJobDao;
 import io.tmgg.modules.job.entity.SysJob;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +21,7 @@ import java.util.List;
 @Component
 @Configuration
 @EnableScheduling
-public class QuartzConfig implements CommandLineRunner {
+public class QuartzInit implements CommandLineRunner {
 
 
 
@@ -43,23 +44,25 @@ public class QuartzConfig implements CommandLineRunner {
             log.warn("定时任务模块已设置全局关闭");
             return;
         }
+        // 兼容性代码
+        if("false".equals(SpringTool.getProperty("sys.job.enable"))){
+            log.warn("【兼容性提示】请将sys.job.enable配置替换为sys.jobEnable");
+            log.warn("定时任务模块已设置全局关闭");
+            return;
+        }
 
 
         // 2. 加载数据库任务
         List<SysJob> list = sysJobDao.findAllEnabled();
         for (SysJob sysJob : list) {
             try {
+                log.info("加载定时任务: {} {}", sysJob.getName(), sysJob.getJobClass());
                 quartzService.scheduleJob(sysJob);
             } catch (ClassNotFoundException e) {
                 log.error("加载数据库任务失败：{}", e.getMessage());
             }
         }
     }
-
-
-
-
-
 
 
 }
