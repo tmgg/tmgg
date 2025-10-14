2e6231ef5d1ceef943fbbd851711f8b66495456c
jiangtao <jiangtao.cctv@qq.com>
2025-10-14 15:45:22 +0800

    部署快照版本

--- script_snapshot.bat
+++ script_snapshot.bat
@@ -1,11 +1,14 @@
 @echo off
 
 :: 参数
-set  "tag_name=1.1.58-SNAPSHOT"
+set  "tag_name=0.1.58"
 
 echo %TIME%  %tag_name% start
 
-call mvnw  clean  deploy    -DskipTests   -P snapshot -Drevision=%tag_name%
+::call mvnw  clean  deploy    -DskipTests   -P snapshot -Drevision=%tag_name%-SNAPSHOT
+
+pnpm -r exec pnpm version %tag_name%
+pnpm publish -r --access public --no-git-checks  --registry http://10.79.43.231:8081/repository/npm/
 
 echo %TIME%  %tag_name% finish
 
