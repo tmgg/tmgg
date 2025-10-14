47988a5cb4ad512ccd6920f4838ab43638151b8c
jiangtao <jiangtao.cctv@qq.com>
2025-10-14 16:17:32 +0800

    部署快照版本 同意修改基础版本未0.0.1024

--- script_snapshot.bat
+++ script_snapshot.bat
@@ -1,14 +1,15 @@
 @echo off
 
 :: 参数
-set  "tag_name=0.1.58"
+set  "tag_name=0.0.520"
 
 echo %TIME%  %tag_name% start
 
-::call mvnw  clean  deploy    -DskipTests   -P snapshot -Drevision=%tag_name%-SNAPSHOT
+call mvnw  clean  deploy    -DskipTests   -P snapshot -Drevision=%tag_name%-SNAPSHOT
 
-pnpm -r exec pnpm version %tag_name%
-pnpm publish -r --access public --no-git-checks  --registry http://10.79.43.231:8081/repository/npm/
+cd web-monorepo
+call pnpm -r
+call pnpm publish -r --access public --no-git-checks  --registry http://10.79.43.231:8081/repository/npm/
 
 echo %TIME%  %tag_name% finish
 
