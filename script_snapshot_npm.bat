5df10df041790d11cc080ffc803d12d1db18dceb
jiangtao <jiangtao.cctv@qq.com>
2025-10-14 17:15:13 +0800

    部署快照版本 同意修改基础版本未0.0.1024

---
+++ script_snapshot_npm.bat
@@ -0,0 +1,18 @@
+chcp 65001
+@echo off
+
+:: 参数
+
+echo %TIME%   start
+
+
+cd web-monorepo
+call pnpm -r exec pnpm version prerelease --preid=beta
+call pnpm publish -r --access public --no-git-checks --tag beta --registry http://10.79.43.231:8081/repository/npm/
+
+echo %TIME%   finish
+
+
+
+
+
