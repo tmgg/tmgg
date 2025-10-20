7f7416ba336aa233463ccfd4b46ecdf422e005e5
jiangtao <jiangtao.cctv@qq.com>
2025-10-20 10:41:07 +0800

    菜单

--- tmgg-system-plugins/tmgg-system-flowable/src/main/java/io/tmgg/system/FlowableMenuInit.java
+++ tmgg-system-plugins/tmgg-system-flowable/src/main/java/io/tmgg/system/FlowableMenuInit.java
@@ -17,7 +17,7 @@ public class FlowableMenuInit implements SystemHook {
     @Override
     public void afterDataInit() {
         sysMenuService.addMenu("biz","flowableTask", "我的任务",null,"flowableTask","/flowable/task", -1,true);
-        sysMenuService.addDir("sys","flowableMgr", "流程引擎",BranchesOutlined,30);
+        sysMenuService.addDir("sys","flowableMgr", "流程引擎",BranchesOutlined,10);
         sysMenuService.addMenu("flowableMgr","flowableModel", "模型管理",null,"flowableModel","/flowable", 1,false);
         sysMenuService.addMenu("flowableMgr","flowableMonitor", "流程监控",null,"flowableMonitor","/flowable/monitor", 2,false);
     }
