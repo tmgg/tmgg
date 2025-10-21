ee74624f032b4e9465b5670cc1822b5a331bb290
jiangtao <jiangtao.cctv@qq.com>
2025-10-21 11:15:48 +0800

    搜索

--- tmgg-base/tmgg-commons-web-framework/src/main/java/io/tmgg/web/persistence/StatField.java
+++ tmgg-base/tmgg-commons-web-framework/src/main/java/io/tmgg/web/persistence/StatField.java
@@ -9,4 +9,16 @@ public class StatField {
     String name;
 
     StatType type;
+
+    public static StatField of(String name, StatType type){
+        return new StatField(name,type);
+    }
+
+    public static StatField sum(String name){
+        return new StatField(name,StatType.SUM);
+    }
+
+    public static StatField count(String name){
+        return new StatField(name,StatType.COUNT);
+    }
 }
