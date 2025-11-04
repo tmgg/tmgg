6f11001ace0f164d3dc6de01498706c622479916
jiangtao <jiangtao.cctv@qq.com>
2025-11-04 16:24:20 +0800

    发布前端测试

---
+++ script_npm_change_version.bat
@@ -0,0 +1,6 @@
+cd web-monorepo
+pnpm -r exec pnpm version 2.0.0-beta.0
+
+
+
+
--- script_npm_publish-beta.bat
+++ script_npm_publish-beta.bat
@@ -1,13 +1,5 @@
-chcp 65001
-@echo off
-
-:: 参数
-
-
-
 cd web-monorepo
 
-:: 设置为下个版本 pnpm -r exec pnpm version 1.1.60-beta.0
 
 call pnpm -r exec pnpm version prerelease --preid=beta
 call pnpm publish -r --access public --no-git-checks --tag beta --registry http://10.79.43.231:8081/repository/npm/
