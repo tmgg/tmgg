388521182c4fc6da339bf013557ba081a0f39c78
jiangtao <jiangtao.cctv@qq.com>
2025-11-05 18:30:54 +0800

    flyway独立

--- tmgg-system/src/main/java/io/tmgg/config/FlywayConfig.java
+++ tmgg-system/src/main/java/io/tmgg/config/FlywayConfig.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 /**
  * flyway默认在jpa建表前执行，这里调整下顺序。
  * 实现思路：
@@ -24,12 +26,25 @@ public class FlywayConfig implements SystemHook {
 
 
     @Bean
-    public SystemHook flywayHook(Flyway flyway){
+    public SystemHook flywayHook(Flyway springFlyway, DataSource ds){
         return  new SystemHook() {
             @Override
             public void onEvent(SystemHookEventType eventType) {
                 if(eventType == SystemHookEventType.BEFORE_DATA_INIT){
+
+                    // 执行框架的flyway, 放在不同的文件夹下的
+                    Flyway flyway = Flyway.configure()
+                            .dataSource(ds)
+                            .locations("classpath:db/migration-framework")
+                            .baselineOnMigrate(true)
+                            .baselineVersion("0")
+                            .table("flyway_schema_history_framework")
+                            .load();
                     flyway.migrate();
+
+
+                    // 执行springboot 自动配置的flyway
+                    springFlyway.migrate();
                 }
             }
         };
