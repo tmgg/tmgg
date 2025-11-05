1291c129a0af51bbe94cb31b5a961413ab588643
jiangtao <jiangtao.cctv@qq.com>
2025-11-05 17:12:43 +0800

    更新模板

---
+++ tmgg-system/src/main/resources/db/migration/framework/R__schema.sql
@@ -0,0 +1,32 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS drop_column_if_exists
+$$
+
+CREATE PROCEDURE drop_column_if_exists(
+    IN p_table_name VARCHAR(64),
+    IN p_column_name VARCHAR(64)
+)
+BEGIN
+    DECLARE column_exists INT;
+
+    -- 检查列是否存在
+    SELECT COUNT(*)
+    INTO column_exists
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = p_table_name
+      AND COLUMN_NAME = p_column_name;
+
+    -- 如果存在，则删除该列
+    IF column_exists > 0 THEN
+        SET @sql = CONCAT('ALTER TABLE ',  p_table_name, ' DROP COLUMN ', p_column_name);
+        PREPARE stmt FROM @sql;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+        SELECT CONCAT('Column "', p_column_name, '" dropped from table "', p_table_name, '".') AS message;
+    ELSE
+        SELECT CONCAT('Column "', p_column_name, '" does NOT exist in table "', p_table_name, '".') AS message;
+    END IF;
+END$$
+
+DELIMITER ;
