c9a4e8b73f66091e475262656e6d9449fafc1893
jiangtao <jiangtao.cctv@qq.com>
2025-11-06 12:29:36 +0800

    去掉支付板块

--- tmgg-system-plugins/tmgg-system-payment/src/main/java/io/tmgg/payment/mgmt/dao/PaymentChannelDao.java
+++
@@ -1,9 +0,0 @@
-package io.tmgg.payment.mgmt.dao;
-
-import io.tmgg.data.repository.BaseDao;
-import io.tmgg.payment.mgmt.entity.PaymentChannel;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class PaymentChannelDao extends BaseDao<PaymentChannel> {
-}
