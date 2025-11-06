000c6e1c2089a73f3464b4b8002a918f7187cbd5
jiangtao <jiangtao.cctv@qq.com>
2025-11-06 12:30:45 +0800

    去掉支付板块

--- tmgg-base/tmgg-commons-web-base/src/main/java/io/tmgg/web/argument/RequestBodyKeys.java
+++ tmgg-base/tmgg-commons-web-base/src/main/java/io/tmgg/web/argument/RequestBodyKeys.java
@@ -1,12 +1,11 @@
 package io.tmgg.web.argument;
 
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class RequestBodyKeys extends ArrayList<String> {
-    public RequestBodyKeys(@NotNull Collection<? extends String> c) {
+    public RequestBodyKeys(Collection<? extends String> c) {
         super(c);
     }
 }
