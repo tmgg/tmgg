a2fa6053a80441e6eecc532f1e613f405c60c573
jiangtao <jiangtao.cctv@qq.com>
2025-11-06 09:49:09 +0800

    高度自适应

--- tmgg-system/src/main/java/io/tmgg/init/SysMenuInit.java
+++ tmgg-system/src/main/java/io/tmgg/init/SysMenuInit.java
@@ -20,29 +20,6 @@ public class SysMenuInit {
 
 
     public void init() throws Exception {
-        sysMenuService.addDir(null,"biz","业务模块", null,0);
-        sysMenuService.addDir(null,"sys","系统管理", null,100);
-
-        sysMenuService.addDir("sys","sysMonitor","系统监控", AntDesignIcon.DesktopOutlined,199);
-
-
-
-
-
-        sysMenuService.addDir("sys","jobMgr","作业调度", AntDesignIcon.ScheduleOutlined,7);
-        sysMenuService.addMenu("jobMgr","job","作业管理", AntDesignIcon.OrderedListOutlined,"job","/job",1,false);
-        sysMenuService.addMenu("jobMgr","jobLog","作业日志", AntDesignIcon.FileOutlined,"jobLog","/job/logList",2,false);
-        sysMenuService.addMenu("jobMgr","jobStatus","作业监控", AntDesignIcon.FundViewOutlined,"jobStatus","/job/status",3,false);
-
-        sysMenuService.addMenu("sys","sysManual","操作手册", AntDesignIcon.CopyOutlined,"sysManual","/system/sysManual",4,false);
-
-
-        initParsers();
-
-
-    }
-
-    private void initParsers() throws Exception {
         Collection<SysMenuParser> parsers = SpringTool.getBeans(SysMenuParser.class);
         for (SysMenuParser parser : parsers) {
             Collection<SysMenu> menus = parser.parseMenuList();
@@ -57,6 +34,8 @@ public class SysMenuInit {
             }
         }
     }
+
+
 
 
 }
--- tmgg-system/src/main/resources/db/migration-framework/system/R__init_data.sql
+++ tmgg-system/src/main/resources/db/migration-framework/system/R__init_data.sql
@@ -31,7 +31,7 @@ INSERT IGNORE sys_menu (id, pid, icon, name, path, seq, type) VALUES ('jobStatus
 INSERT IGNORE sys_menu (id, pid, icon, name, path, seq, type) VALUES ('jobLog', 'jobMgr', 'FileOutlined', '作业日志', '/job/logList', 2, 'MENU');
 
 
-INSERT IGNORE sys_menu (id, pid, icon, name, path, seq, type) VALUES ('machine', 'sysMonitor', NULL, '服务监控', '/system/machine', 1, 'MENU');
+delete from sys_menu where id = 'machine';
 INSERT IGNORE sys_menu (id, pid, icon, name, path, seq, type) VALUES ('dev', 'sys', 'JavaOutlined', '开发管理', '/code', 200, 'MENU');
 
 INSERT IGNORE sys_menu (id, pid, icon, name, path, seq, type) VALUES ('api', 'sys', 'ApiOutlined', '开放接口', NULL, 201, 'DIR');
