27947724ed04c6ceb6179d4906b27857e96c19eb
jiangtao <jiangtao.cctv@qq.com>
2025-11-06 16:55:46 +0800

    本地复制包

--- script_npm_copy.bat
+++ script_npm_copy.bat
@@ -4,11 +4,11 @@ set  "target=D:\ws\wuzi\web\node_modules"
 
 cd web-monorepo
 
-call robocopy "packages/tmgg-commons-lang" "%target%/@tmgg/tmgg-commons-lang" /E
-call robocopy "backend/tmgg-base" "%target%/@tmgg/tmgg-base" /E
-call robocopy "backend/tmgg-system" "%target%/@tmgg/tmgg-system" /E
+call robocopy "packages/tmgg-commons-lang" "%target%/@tmgg/tmgg-commons-lang" /E /XD "node_modules"
+call robocopy "backend/tmgg-base" "%target%/@tmgg/tmgg-base" /E /XD "node_modules"
+call robocopy "backend/tmgg-system" "%target%/@tmgg/tmgg-system" /E /XD "node_modules"
 
-call robocopy "backend-plugins/tmgg-system-flowable" "%target%/tmgg-system-flowable" /E
+call robocopy "backend-plugins/tmgg-system-flowable" "%target%/tmgg-system-flowable" /E /XD "node_modules"
 
-call robocopy "backend-plugins/tmgg-system-ureport" "%target%/tmgg-system-ureport" /E
+call robocopy "backend-plugins/tmgg-system-ureport" "%target%/tmgg-system-ureport" /E /XD "node_modules"
 
