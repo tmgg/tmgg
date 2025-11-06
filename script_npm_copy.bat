872d8f6af4134c82f56ebae050b2629a102ad824
jiangtao <jiangtao.cctv@qq.com>
2025-11-06 16:53:31 +0800

    本地复制包

--- script_npm_copy.bat
+++ script_npm_copy.bat
@@ -8,5 +8,7 @@ call robocopy "packages/tmgg-commons-lang" "%target%/@tmgg/tmgg-commons-lang" /E
 call robocopy "backend/tmgg-base" "%target%/@tmgg/tmgg-base" /E
 call robocopy "backend/tmgg-system" "%target%/@tmgg/tmgg-system" /E
 
+call robocopy "backend-plugins/tmgg-system-flowable" "%target%/tmgg-system-flowable" /E
 
+call robocopy "backend-plugins/tmgg-system-ureport" "%target%/tmgg-system-ureport" /E
 
